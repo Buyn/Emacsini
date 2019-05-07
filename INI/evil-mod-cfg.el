778690796859dc5803b8221b4dab4be7fa5febdc
buyn <buyn.tex@gmail.com>
2019-05-07 10:53:11 +0300

    coments refactoring

--- INI/evil-mod-cfg.el
+++ INI/evil-mod-cfg.el
@@ -6,26 +6,32 @@
 ;; --------------------------------------
 (require 'evil)
 (evil-mode t)
+;; --------------------------------------
 ;; (define-key key-translation-map (kbd "ch") (kbd "C-h"))
 ;; (define-key evil-normal-state-map "c" nil)
 ;; (define-key evil-motion-state-map "cu" 'universal-argument)
+;; --------------------------------------
 ;; Movements
 (define-key evil-normal-state-map "gl" 'move-end-of-line)
 (define-key evil-normal-state-map "gh" 'move-beginning-of-line)
 (define-key evil-normal-state-map (kbd "gj") 'evil-next-visual-line)
 (define-key evil-normal-state-map (kbd "gk") 'evil-previous-visual-line)
-;;Frams Controls
-(define-key evil-normal-state-map "gtt" 'make-frame-command)
-(define-key evil-normal-state-map "gtf" 'find-file-other-frame)
-(define-key evil-normal-state-map "gtb" 'switch-to-buffer-other-frame)
-(define-key evil-normal-state-map "gtj" 'other-frame)
-(define-key evil-normal-state-map "gtk" 'previous-multiframe-window)
-;;(define-key evil-normal-state-map "gth" 'other-frame -2)
+;; --------------------------------------
+;; *** Frams Controls ***
+(define-key evil-normal-state-map (kbd "M-g M-t M-t") 'make-frame-command)
+;; (define-key evil-normal-state-map "gtt" 'make-frame-command)
+(define-key evil-normal-state-map (kbd " M-g M-t M-f") 'find-file-other-frame)
+(define-key evil-normal-state-map (kbd " M-g M-t M-b") 'switch-to-buffer-other-frame)
+;; --------------------------------------
+;;   ***   Help in New Frame   ***
+;; (define-key evil-normal-state-map (kbd " M-g M-t M-j") 'other-frame)
+;; (define-key evil-normal-state-map (kbd " M-g M-t M-k") 'previous-multiframe-window)
+;; ;;(define-key evil-normal-state-map "gth" 'other-frame -2)
 ;;(define-key evil-normal-state-map "gtl" 'other-frame 2)
-(define-key evil-normal-state-map "gthk" 'describe-key)
-(define-key evil-normal-state-map "gthf" 'describe-function)
+(define-key evil-normal-state-map (kbd " M-g M-t M-h M-k") 'describe-key)
+(define-key evil-normal-state-map (kbd " M-g M-t M-h M-f") 'describe-function)
+;; --------------------------------------
 ;; Frams Close
-
 (defun kill-buffer-and-frame ()
   "kill buffer and cloze frame"
   (interactive)
@@ -33,4 +39,8 @@
   (kill-this-buffer)
   (evil-quit))
 (define-key evil-normal-state-map "ZX" 'kill-buffer-and-frame)
+;; --------------------------------------
+;; EVIL TABS
+(global-evil-tabs-mode t)
+;; :tabnew
 
