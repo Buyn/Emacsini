064f409356d2a479281606222de917a3702f08d7
buyn <buyn.tex@gmail.com>
2020-01-27 04:05:08 +0200

    fist outshine config

--- INI/outshine_cfg.el
+++ INI/outshine_cfg.el
@@ -6,5 +6,12 @@
 ;; OUTSHINE-MOD CUSTOMIZATION {{{
 ;; --------------------------------------
 (use-package outshine :ensure t
-  :quelpa (outshine :fetcher github :repo "alphapapa/outshine"))
+  :quelpa (outshine :fetcher github :repo "alphapapa/outshine")
+  :config
+	;; (defvar outline-minor-mode-prefix "\M-#")
+	(add-hook 'outline-minor-mode-hook 'outshine-hook-function)
+	(add-hook 'emacs-lisp-mode-hook 'outline-minor-mode)
+	;; (add-hook 'LaTeX-mode-hook 'outline-minor-mode)
+	(add-hook 'python-mode-hook 'outline-minor-mode)
+	)
 ;; -------------------------------------- }}}
