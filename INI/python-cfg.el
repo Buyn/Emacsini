688f83dd8aaf656995ad35a08c70940c792714e8
BuYn <buyn.tex@gmail.com>
2020-02-28 10:19:03 +0200

    add f8 recompile

--- INI/python-cfg.el
+++ INI/python-cfg.el
@@ -10,22 +10,34 @@
 ;; ** (use-package elpy
 (require 'use-package)
 (use-package elpy :ensure t
+;; *** init : 
 	:defer t
 	:init
 	(elpy-enable)
+;; *** config : 
 	:config
 	;; (elpy-enable)
 	;; (ivy-mode 1)
 	(company-mode 1)
+	(setq compilation-scroll-output nil)
 	;; not working from config onli from init and not from hooks
 	;; (add-hook 'python-mode-hook 'elpy-mode)
 	;; (add-hook 'python-mode-hook 'elpy-enable)
 	;; (setq python-shell-interpreter "python"
 	;; python-shell-interpreter-args "-i")
 	;; findout what it do
+;; *** define-key : 
 	(define-key evil-insert-state-map (kbd "C-SPC") 'company-complete)
 	(define-key evil-normal-state-map (kbd "M-e M-f M-p") 'elpy-goto-definition)
 	(define-key evil-normal-state-map (kbd "M-e M-f M-P") 'elpy-goto-definition-other-window)
+	(define-key evil-insert-state-map (kbd "<f8>") '(lambda() (interactive)
+
+				(org-save-all-org-buffers)
+				(save-some-buffers 'no-confirm)
+				(evil-normal-state)
+				(recompile)
+				))
+;; *** 	) : 
 	)
 ;; ** (use-package company-jedi 
 ;; (use-package company-jedi 
