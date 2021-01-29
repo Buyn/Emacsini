95c68113ac9c7ac8bd43b6c2e26dd0ff4d830882
buyn <buyn.tex@gmail.com>
2021-01-29 22:29:43 +0200

    add artist keys f1-f7

--- INI/ORG/graph-tools-cfg.el
+++ INI/ORG/graph-tools-cfg.el
@@ -34,7 +34,22 @@
 ;;  --------------------------------------
 
 ;; * ARTIST MODE
-
+(add-hook 'artist-mode-hook (lambda ()
+	;; (local-set-key (kbd "<f1>") 'org-mode)
+	(local-set-key (kbd "<f2>") 
+		'artist-select-op-pen-line) ; f2 = pen mode
+	(local-set-key (kbd "<f3>") 
+		'artist-select-op-line)     ; f3 = line
+	(local-set-key (kbd "<f4>") 
+		'artist-select-op-square) ; f4 = rectangle
+	(local-set-key (kbd "<f5>") 
+		'artist-select-op-ellipse)  ; f5 = ellipse
+	(local-set-key (kbd "<f6>") 
+		'artist-select-op-erase-rectangle)  ; f6 = erase
+	(local-set-key (kbd "<f7>") 
+		'artist-select-op-spray-can)  ; f7 = spray
+	;; (local-set-key (kbd "C-z") 'undo)
+	))
 ;; ** --------------------------------------
 
 ;; * --------------------------------------
