049ee917c79072e444cf08273d53361145c60c03
buyn <buyn.tex@gmail.com>
2021-04-12 13:46:28 +0300

    add 4 lengs 2 ru (milina,tania)2 en

--- INI/lit-cfg/read-aloud-cfg.el
+++ INI/lit-cfg/read-aloud-cfg.el
@@ -17,52 +17,70 @@
   :config
 ;; **** plist read-aloud-engines : 
 ;; ***** ru : 
-;; ******  elena : 
+;; ****** elena : 
 (lax-plist-put read-aloud-engines "bal.ru.elena"
 	'(cmd "~/AddApps/balcon/balcon.exe"
 		args ( "-n" "Elena" "-i" "-enc" "utf8")))
-;; ******  Alyona22k : 
+;; ****** Alyona22k : 
 (lax-plist-put read-aloud-engines "bal.ru.Alyona22k"
 	'(cmd "~/AddApps/balcon/balcon.exe"
 		args ( "-n" "Alyona22k" "-i" "-enc" "utf8")))
-;; ******  anna : 
+;; ****** anna : 
 (lax-plist-put read-aloud-engines "bal.ru.anna"
 	'(cmd "~/AddApps/balcon/balcon.exe"
 		args ( "-n" "RHVoice Anna" "-i" "-enc" "utf8")))
-;; ******  Irina : 
+;; ****** Irina : 
 (lax-plist-put read-aloud-engines "bal.ru.Irina"
 	'(cmd "~/AddApps/balcon/balcon.exe"
 		args ( "-n" "Irina" "-i" "-enc" "utf8")))
-;; ******  Katerina : 
+;; ****** Katerina : 
 (lax-plist-put read-aloud-engines "bal.ru.Katerina"
 	'(cmd "~/AddApps/balcon/balcon.exe"
 		args ( "-n" "Katerina" "-i" "-enc" "utf8")))
+;; ****** Tatyana : 
+(lax-plist-put read-aloud-engines "bal.ru.Tatyana"
+	'(cmd "~/AddApps/balcon/balcon.exe"
+		args ( "-n" "Tatyana" "-i" "-enc" "utf8")))
+;; ****** Milena : 
+(lax-plist-put read-aloud-engines "bal.ru.Milena"
+	'(cmd "~/AddApps/balcon/balcon.exe"
+		args ( "-n" "Milena" "-i" "-enc" "utf8")))
 ;; ***** ua : 
 ;; ******  Natalia : 
 (lax-plist-put read-aloud-engines "bal.ua.Natalia"
 	'(cmd "~/AddApps/balcon/balcon.exe"
 		args ( "-n" "Natalia" "-i" "-enc" "utf8")))
 ;; ***** en : 
-;; ******  Slt : 
+;; ****** Slt : 
 (lax-plist-put read-aloud-engines "bal.en.Slt"
 	'(cmd "~/AddApps/balcon/balcon.exe"
 		args ( "-n" "Slt" "-i" "-enc" "utf8")))
-;; ******  Bdl : 
+;; ****** Bdl : 
 (lax-plist-put read-aloud-engines "bal.en.Bdl"
 	'(cmd "~/AddApps/balcon/balcon.exe"
 		args ( "-n" "Bdl" "-i" "-enc" "utf8")))
-;; ******  Clb : 
+;; ****** Clb : 
 (lax-plist-put read-aloud-engines "bal.en.Clb"
 	'(cmd "~/AddApps/balcon/balcon.exe"
 		args ( "-n" "Clb" "-i" "-enc" "utf8")))
-;; ******  jampal.en : 
+
+;; ****** Heather
+	(lax-plist-put read-aloud-engines "bal.en.Heather"
+		'(cmd "~/AddApps/balcon/balcon.exe"
+			args ( "-n" "Heather" "-i" "-enc" "utf8")))
+;; ****** Lucy
+(lax-plist-put read-aloud-engines "bal.en.Lucy"
+	'(cmd "~/AddApps/balcon/balcon.exe"
+			args ( "-n" "Lucy" "-i" "-enc" "utf8")))
+;; ****** jampal.en : 
 	(lax-plist-put read-aloud-engines "jampal.en"
 		'(cmd "cscript"
 					args ("D:\\Development\\lisp\\Home\\AddApps\\Jampal\\ptts.vbs" "-r" "1")))
 ;; **** setq read-aloud-engine : 
 ;; (setq read-aloud-engine "jampal.en"))
 ;; (setq read-aloud-engine "balcon.ru")
-	(setq read-aloud-engine "bal.en.Slt")
+;; (setq read-aloud-engine "bal.en.Slt")
+	(setq read-aloud-engine "bal.en.Lucy")
 ;; *** ===========END========= ) : 
 	)
 ;; ** Docs
