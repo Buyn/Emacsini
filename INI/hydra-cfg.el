ac2036fb4fd1b0354cf742327e609a8e4b8cb6a9
buyn <buyn.tex@gmail.com>
2022-02-06 09:11:01 +0200

    fix hidra slowin spid

--- INI/hydra-cfg.el
+++ INI/hydra-cfg.el
@@ -751,7 +751,7 @@ _Y_ankPageUrl  _f_rameLink              _z_oom		_q_uit
 ;; **** B : 
 	("B" read-aloud-buf "aloudBuffer")
 ;; **** s : 
-	("s" read-aloud-stop "StopAloud")
+	("S" read-aloud-stop "StopAloud")
 ;; **** E : 
 	("E" read-aloud-change-engine "AloudEngine")
 ;; **** q : 
