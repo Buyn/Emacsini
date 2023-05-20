0556ff9c5dff6d206101410c0f1aa9884d596c58
buyn <buyn.tex@gmail.com>
2023-05-20 10:34:30 +0300

    org error elpa

--- INI/inpak.el
+++ INI/inpak.el
@@ -31,8 +31,8 @@ There are two things you can do about this warning:
 ;; (add-to-list 'package-archives
 ;; ;;        '("melpa" . "http://melpa.org/packages/") t)
 ;;        '("melpa" . "http://melpa.org/packages/") )
-(add-to-list 'package-archives 
-		 '("org" . "http://orgmode.org/elpa/") t)
+;; (add-to-list 'package-archives 
+;; 		 '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives 
 		 '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 ;; --------------------------------------
