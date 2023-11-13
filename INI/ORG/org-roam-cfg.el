2ca03be5a29c6476c533ee6f7cb60f781561880b
buyn <buyn.tex@gmail.com>
2023-11-13 19:41:01 +0200

    fix Org-Roam server

--- INI/ORG/org-roam-cfg.el
+++ INI/ORG/org-roam-cfg.el
@@ -52,7 +52,8 @@
 ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
 ;;         a hookable mode anymore, you're advised to pick something yourself
 ;;         if you don't care about startup time, use
-	:hook (after-init . org-roam-ui-mode)
+	;; запускается сервер сразу с имаком
+	;; :hook (after-init . org-roam-ui-mode)
 	:config
 	(setq 
 				;; Org-Roam-UI can sync your Emacs theme! This is the default behavior, to disable it do
